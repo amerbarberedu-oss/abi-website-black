5c67c06ce519eae11890e4d406cacbf785a87852
American Barber Institute <web@abi.edu>
2026-07-04 05:00:13 +0530

    chore: drop dead Alex chatbot.css link from landing funnels (GHL widget is live chatbot)

--- landing-funnels/src/build.py
+++ landing-funnels/src/build.py
@@ -769,7 +769,6 @@ def page_head(p):
 '<link rel="preload" href="/assets/img/lf-hero.jpg" as="image" media="(min-width:769px)">\n'
 '<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">\n'
 '<link rel="stylesheet" href="/assets/css/funnels.css?v=%(cssv)s">\n'
-'<link rel="stylesheet" href="/assets/css/chatbot.css?v=%(cssv)s">\n'
 '%(ld_scripts)s'
 '</head>\n<body class="lf-page %(theme)s">\n'
     ) % {
